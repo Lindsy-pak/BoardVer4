f30f92d2f008b5453ba8f1a2f861a1f6dcd8d861
Lindsy-pak <81272479+Lindsy-pak@users.noreply.github.com>
2021-05-11 16:56:16 +0900

    20210511

---
+++ BoardVer4/src/main/java/com/Lindsy/board4/MyUtils.java
@@ -0,0 +1,29 @@
+package com.Lindsy.board4;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyUtils {
+	public static void openJSP(String fileNm, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String jsp = "/WEB-INF/view/" + fileNm + ".jsp";
+		request.getRequestDispatcher(jsp).forward(request, response);
+	}
+	
+	public static int parseStringToInt(String val) {
+		try {
+			int result = Integer.parseInt(val);
+			return result;
+		} catch(Exception e) {
+			return 0;
+		}
+	}
+	
+	public static int getParamInt(String key, HttpServletRequest request) {
+		String val = request.getParameter(key);
+		int intVal = parseStringToInt(val);
+		return intVal;
+	}
+}
